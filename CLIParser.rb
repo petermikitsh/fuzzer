3e4263cd0536c8ad892433484198a2c50c49fb4e
Matt Stevens <majold604@gmail.com>
2014-09-18 13:21:20 -0400

    Expanded CLIParser

--- CLIParser.rb
+++ CLIParser.rb
@@ -1,9 +1,21 @@
 class CLIParser
 
 	def self.parse
-		ARGV.each do|a| 
-			puts "Argument: #{a}" 
-		end 
+		#ruby fuzz [discover | test] url OPTIONS 
+		if (ARGV[0] == 'discover')
+			puts 'discovery'
+		end
+		if (ARGV[0] == 'test')
+			puts 'testdat'
+		end
+		testURL = ARGV[1] #URL to test
+		ARGV[2..-1].each do|arg|#Other options
+			puts arg
+		end
+		#ARGV.each do|a| 
+		#	puts "Argument: #{a}"
+		#end 
 	end
 
+	parse
 end
