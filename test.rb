4a985f49d0537b437fe493a396a845eef25162f5
Peter Mikitsh <peter.mikitsh@gmail.com>
2014-10-13 17:43:15 -0400

    test.rb: catch unexpected response codes

--- test.rb
+++ test.rb
@@ -6,14 +6,33 @@
 #
 # Author: Peter Mikitsh pam3961
 
+require 'mechanize'
+
 class Test
 
 	# urls: string array of absolute URL paths to test
 	# vectors: string array of replacive vectors to append to urls
 	# authAgent: optional 'Mechanize' agent (if authentication used
-	
-	def self.test(urls, vectors, authAgent)
 
+	def self.test(urls, vectors, authAgent)
+		agent = authAgent ? authAgent : Mechanize.new
+		urls.each do |url|
+			vectors.each do |vector|
+				Test.replaciveFuzz(url, vector, agent)
+			end
+		end
+	end
+
+	def self.createAttackURL(url, vector)
+		return url + vector;
+	end
+
+	def self.replaciveFuzz(url, vector, agent)
+		begin
+		  agent.get(Test.createAttackURL(url, vector))
+		rescue Mechanize::ResponseCodeError
+			puts "Unexcepted response code."
+		end
 	end
 
 end
